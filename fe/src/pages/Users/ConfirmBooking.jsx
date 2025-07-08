a5f2bbd81461c0326de8558395dd294a3eab0c5b
Nguyễn Tân Thuận <thuanntse183670@fpt.edu.vn>
2025-07-08 20:51:20 +0700

    Fix confirm booking

--- fe/src/pages/Users/ConfirmBooking.jsx
+++ fe/src/pages/Users/ConfirmBooking.jsx
@@ -265,12 +265,10 @@ const ConfirmBooking = () => {
             <div className="text-sm border-t border-gray-600 mt-1 pt-2 space-y-1">
               <p><span className="text-gray-400">Full Name:</span> {userData.name}</p>
               <p><span className="text-gray-400">Email:</span> {userData.email}</p>
-              <p><span className="text-gray-400">ID Number:</span> {userData.id}</p>
               <p><span className="text-gray-400">Phone:</span> {userData.phone}</p>
               {/* Add other user details from Redux/fetched data */}
               <p><span className="text-gray-400">Username:</span> {userData.username}</p>
               <p><span className="text-gray-400">Gender:</span> {userData.gender}</p>
-              <p><span className="text-gray-400">Address:</span> {userData.address}</p>
             </div>
 
             {/* Voucher Input */}
